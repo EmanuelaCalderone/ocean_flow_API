ccad2ce568b6f0f607e01c6f8501f118f67d0877
DamianoCelesti <damiano.celesti@gmail.com>
2025-03-28 01:05:40 +0100

    add all img and fix searchcontoller

--- controllers/searchControllers.js
+++ controllers/searchControllers.js
@@ -26,7 +26,7 @@ const search = (req, res) => {
 
         if (err) return res.status(500).json({ error: 'Database query failed' });
 
-        if (results.length === 0) { return res.status(404).json({ message: 'Product not found' }); }
+        if (results.length === 0) { return res.json([]); }
 
         res.json(results);
 
